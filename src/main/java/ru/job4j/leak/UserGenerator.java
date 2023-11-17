eb4eb9b07e0db6cef37c66bda914dc35b68817da
Yamijke <serhei.knyazev@gmail.com>
2023-11-17 13:14:23 +0300

    4. Найти утечку памяти. [#504882 #457022]

--- src/main/java/ru/job4j/leak/UserGenerator.java
+++ src/main/java/ru/job4j/leak/UserGenerator.java
@@ -28,10 +28,13 @@ public class UserGenerator implements Generate {
     public void generate() {
         users.clear();
         for (int i = 0; i < NEW_USERS; i++) {
-            users.add(new User(
-                    surnames.get(random.nextInt(surnames.size())) + SEPARATOR
-                            + names.get(random.nextInt(names.size())) + SEPARATOR
-                            + patrons.get(random.nextInt(patrons.size()))));
+            StringBuilder name = new StringBuilder();
+            name.append(surnames.get(random.nextInt(surnames.size())))
+                    .append(SEPARATOR)
+                    .append(names.get(random.nextInt(names.size())))
+                    .append(SEPARATOR)
+                    .append(patrons.get(random.nextInt(patrons.size())));
+            users.add(new User(name.toString()));
         }
     }
 
